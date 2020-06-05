41ea123fdcb6dccc10d4996ab6e068bb0e6c27c8
Gino Marciano <gino.bolt@hva.nl>
2020-06-05 16:23:21 +0200

    attributen advert en user voor locatie

--- app/src/main/java/com/example/rentshare/model/Advert.java
+++ app/src/main/java/com/example/rentshare/model/Advert.java
@@ -7,12 +7,25 @@ public class Advert {
     private String description;
     private long price;
     private String imageUrl;
+    private String latitude;
+    private String longitude;
+    private long userId;
 
     public Advert(String title, String description, long price, String imageUrl) {
         this.title = title;
         this.description = description;
         this.price = price;
         this.imageUrl = imageUrl;
+    }
+
+    public Advert(String title, String description, long price, String imageUrl, String latitude, String longitude, long userId) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.userId = userId;
     }
 
     public int getId() {
@@ -54,4 +67,16 @@ public class Advert {
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
+
+    public String getLatitude() { return latitude; }
+
+    public void setLatitude(String latitude) { this.latitude = latitude; }
+
+    public String getLongitude() { return longitude; }
+
+    public void setLongitude(String longitude) { this.longitude = longitude; }
+
+    public long getUserId() { return userId; }
+
+    public void setUserId(long userId) { this.userId = userId; }
 }
