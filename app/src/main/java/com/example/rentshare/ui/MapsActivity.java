4e00fb5fd215cfb0fc55db6dcf32fe590c222cd5
Gino Marciano <gino.bolt@hva.nl>
2020-06-16 23:45:05 +0200

    Gps in add en Maps in details

--- app/src/main/java/com/example/rentshare/ui/MapsActivity.java
+++ app/src/main/java/com/example/rentshare/ui/MapsActivity.java
@@ -1,14 +1,10 @@
 package com.example.rentshare.ui;
 
 import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import com.example.rentshare.R;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -31,7 +27,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         longitude = intentToken.getExtras().getDouble("longitude");
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(R.id.mapOld);
         mapFragment.getMapAsync(this);
 
     }
